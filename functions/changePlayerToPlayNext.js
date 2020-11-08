64415e0ba3669cd26a24a64286271c4e82b1838d
Kenedy-Henrique <kenedyhenrique.buenosilva@gmail.com>
2020-11-08 18:12:56 -0300

    loop fix

--- functions/changePlayerToPlayNext.js
+++ functions/changePlayerToPlayNext.js
@@ -1,6 +1,9 @@
 module.exports = (room) => {
-  if (room[0].playerToPlayNext === room.length) {
-    room[0].playerToPlayNext === 1;
+  if (room[0].playerToPlayNext >= room.length - 1) {
+    //That verify if the player that has now been requested to make a question is the last
+    //if so the next player is set to 1
+    //if not the next player is set to its current value plus 1
+    room[0].playerToPlayNext = 1;
   } else {
     room[0].playerToPlayNext = room[0].playerToPlayNext + 1;
   }
