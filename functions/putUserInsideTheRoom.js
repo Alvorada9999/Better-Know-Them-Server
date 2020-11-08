e12d0d1acecd0a627accedc585ea24b7e80096f8
Kenedy-Henrique <kenedyhenrique.buenosilva@gmail.com>
2020-11-07 21:44:47 -0300

    This put the user inside it's room.

--- functions/putUserInsideTheRoom.js
+++ functions/putUserInsideTheRoom.js
@@ -1,6 +1,9 @@
+const startTheGame = require("./startTheGame");
+
 module.exports = (rooms, roomName, ws) => {
   rooms[roomName].push({
     player: ws,
     choice: "",
   });
+  startTheGame(rooms[roomName]);
 };
