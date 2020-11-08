83877e39f41af61909aba85fd66ef6d4bdbd6a85
Kenedy-Henrique <kenedyhenrique.buenosilva@gmail.com>
2020-11-08 18:13:23 -0300

    log add

--- functions/putUserInsideTheRoom.js
+++ functions/putUserInsideTheRoom.js
@@ -3,7 +3,8 @@ const startTheGame = require("./startTheGame");
 module.exports = (rooms, roomName, ws) => {
   rooms[roomName].push({
     player: ws,
-    choice: "",
+    answer: "This player has not sent any answer",
   });
+  console.log(`The user has been put inside it's room\n`);
   startTheGame(rooms[roomName]);
 };
