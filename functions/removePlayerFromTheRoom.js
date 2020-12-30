d935c1840d58d3a79548e85c56d2017063e56f06
Kenedy-Henrique <kenedyhenrique.buenosilva@gmail.com>
2020-12-30 05:20:04 -0300

    bug fix: looking for a room that does not exists anymore

--- functions/removePlayerFromTheRoom.js
+++ functions/removePlayerFromTheRoom.js
@@ -1,17 +1,19 @@
 module.exports = (room, ws) => {
   let i = 1;
-  while (i <= room.length - 1) {
-    //Begins at position 1 because the position 0 contains room data
-    if (room[i].player === ws) {
-      console.log("One player is being removed from the room");
-      console.log("Room before the player removal:");
-      console.log(room);
-      room.splice(i, 1);
-      console.log("One player has been removed from the room");
-      console.log("Room after the player removal:");
-      console.log(room);
-      break;
+  if (room != undefined) {
+    while (i <= room.length - 1) {
+      //Begins at position 1 because the position 0 contains room data
+      if (room[i].player === ws) {
+        console.log("One player is being removed from the room");
+        console.log("Room before the player removal:");
+        console.log(room);
+        room.splice(i, 1);
+        console.log("One player has been removed from the room");
+        console.log("Room after the player removal:");
+        console.log(room);
+        break;
+      }
+      i++;
     }
-    i++;
   }
 };
